dc336ad6a62334c69c421116167296b8b4bea602
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-11-04 12:12:00 +0200

    ex00

---
+++ d01/ex00/var.py
@@ -0,0 +1,9 @@
+def my_var():
+    var_list = [42,'42','quarante-deux',42.0,True,[42],{42:42},(42,),set()]
+    for var in var_list:
+        print(f'{var} est de type {type(var)}')
+
+
+
+if __name__ == '__main__':
+    my_var()
