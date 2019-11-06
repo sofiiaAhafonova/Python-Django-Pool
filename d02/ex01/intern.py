dcc623c0a006facd23b3a62185e78b48daf98222
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-11-06 11:29:41 +0200

    ex01

---
+++ d02/ex01/intern.py
@@ -0,0 +1,28 @@
+class Intern:
+    def __init__(self, name="My name? I’m nobody, an intern, I have no name."):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+    
+    class Coffee:
+        def __str__(self):
+            return "This is the worst coffee you ever tasted."
+    
+    def work(self):
+        raise Exception("I’m just an intern, I can’t do that...")
+
+    def make_coffee(self):
+        return self.Coffee()
+
+
+if __name__ == '__main__':
+	first = Intern()
+	print(first)
+	second = Intern("Mark")
+	print(second)
+	print(second.make_coffee())
+	try:
+		print(first.work())
+	except Exception as e:
+		print(e)
