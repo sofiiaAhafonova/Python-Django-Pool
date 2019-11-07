c860af5ba755f1c2661ba49107fb90bde8ee1e6f
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-11-07 13:51:20 +0200

    fix ex03

--- d03/ex03/roads_to_philosophy.py
+++ d03/ex03/roads_to_philosophy.py
@@ -2,42 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-
-def filter(tag):
-    return tag is not None and not tag.has_attr(
-        "role") and not tag.name == "abbr" and not tag.name == "img" and tag.name == 'a'
-
-
-def wiki(string_to_find):
-    req = requests.get("https://en.wikipedia.org/wiki/{}".format(string_to_find))
-
-    if req.status_code != 200 or not req:
-        exit("Something wrong with Server")
-
-    if not req.text:
-        exit(f"Can't find {string_to_find}")
-
-    soup = BeautifulSoup(req.text, 'html.parser')
-
-    et_de_un = soup.find('div', attrs={"id": u"bodyContent"})\
-        .find('div', attrs={"id": u"mw-content-text"})\
-        .find('div', attrs={"class": u"mw-parser-output"})
-    for child in et_de_un.children:
-        if child.name == "p":
-            les_a = child.find_all(filter)
-            for a in les_a:
-                href = str(a.get('href'))
-                if href.startswith("/wiki/") \
-                        and not (href.startswith("#")
-                                 or href.startswith("/Wikipedia:")
-                                 or href.startswith("/wiki/Help")
-                                 or href.startswith('/wiki/File')
-                                 or href.startswith('/wiki/Wikipedia:Citation_needed')
-                                 or href.startswith('//upload.wikimedia.org/')):
-                    # print("href " + href.replace("/wiki/", ""))
-                    return href.replace("/wiki/", "")
-
-
 def main():
     if len(sys.argv) != 2:
         exit("Usage: python3 roads_to_philosophy.py string_to_find")
@@ -47,24 +11,58 @@ def main():
     if string_to_find == "Philosophy":
         print("0 roads from Philosophy to philosophy !")
         exit(0)
+    nexturl = "http://en.wikipedia.org/wiki/"+string_to_find
 
-    print(string_to_find)
-    tab_philo = [string_to_find]
-    visited = wiki(string_to_find)
-    while visited is not None:
-        string_to_find.replace(' ', '_')
-        print(visited)
-        tab_philo.append(visited)
-        visited = wiki(visited)
+    visited = []
+    i = 0
+    try:
+        while True:
+            i += 1
+            req = requests.get(nexturl)
 
-        if visited == "Philosophy":
-            iteration = len(tab_philo)
-            print("{} roads from {} to Philosophy !".format(iteration, string_to_find))
-            sys.exit(0)
-        elif visited in tab_philo:
-            print("It leads to an infinite loop ! ")
-            sys.exit()
+            if req.status_code != 200 or not req:
+                exit("Something wrong with Server")
 
+            if not req.text:
+                exit(f"Can't find {string_to_find}")
+
+            soup = BeautifulSoup(req.text, 'html.parser')
+            article = soup.findAll('h1', {'class':'firstHeading'})[0].contents[0]
+            print(article)
+            
+            if article == "Philosophy":
+                break
+            maincontent = soup.findAll("div", { "class" : "mw-parser-output" })[0]
+            paragraphs = maincontent.findAll('p')
+            next_url_found = False
+            for paragraph in paragraphs:
+                if not next_url_found:
+                    parenthesis_in_progress = False
+                    for thing in paragraph.contents:
+                        if not next_url_found:
+                            if "(" in thing:
+                                parenthesis_in_progess = True
+                            if ")" in thing and parenthesis_in_progress:
+                                parenthesis_in_progress = False
+
+                            if not parenthesis_in_progress:
+                                if not isinstance(thing, str):
+                                    try:
+                                        n = thing['href']
+                                        if not n in visited:
+                                            next_url_found = True
+                                            nexturl = n
+                                            visited.append(nexturl)
+                                        else:
+                                            exit("It leads to an infinite loop ! ")
+                                    except:
+                                        pass
+                                    
+            nexturl = "http://en.wikipedia.org" + nexturl
+    
+        print("{} roads from {} to Philosophy !".format(i, string_to_find))
+    except:
+        exit("It leads to a dead end!")
 
 if __name__ == '__main__':
     main()
