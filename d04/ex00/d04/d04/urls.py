66a48623c58bc412f4c550d92ac74b2e3b875cab
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-11-08 09:04:26 +0200

    ex00

--- d04/ex00/d04/d04/urls.py
+++ d04/ex00/d04/d04/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from ex00 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('ex00/', include('ex00.urls'))
 ]
